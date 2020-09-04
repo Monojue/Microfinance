58967b29f48ba912fa89c8f29f589ea0fade9734
Monojue <monojue.km@gmail.com>
2020-09-04 15:51:14 +0630

    MyQueries perpare

--- Microfinance(Github)/src/database/MyOueries.java
+++ Microfinance(Github)/src/database/MyOueries.java
@@ -1,5 +1,60 @@
 package database;
 
-public class MyOueries {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import database.DBConnection;
+
+public class MyOueries {
+	static Connection con = null;
+	static Statement stmt;
+	static String query,query1;
+	ResultSet rs;
+	DBConnection connect = new DBConnection();
+	
+	public MyOueries() {
+		try {
+			con = connect.GetMySQLConnection();
+		}catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}catch (SQLException e) {
+			System.out.println(e);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+/////////////////////// Client Query Start //////////////////////////////////////
+	
+	
+	
+	
+/////////////////////// Client Query End //////////////////////////////////////
+	
+/////////////////////// Group Query Start //////////////////////////////////////
+	
+	
+	
+	
+	
+/////////////////////// Group Query End //////////////////////////////////////
+	
+/////////////////////// LoanRequest Query Start //////////////////////////////////////
+	
+	
+	
+	
+/////////////////////// LoanRequest Query End //////////////////////////////////////
+	
+/////////////////////// GroupLoan Query Start //////////////////////////////////////
+	
+	
+	
+	
+/////////////////////// GroupLoan Query End //////////////////////////////////////
+	
+	
+	
 }
