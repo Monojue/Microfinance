0ffaa9b539d12b6493422fa80828294fb484f90a
Monojue <monojue.km@gmail.com>
2020-09-04 18:15:22 +0630

    Add Uqureies

---
+++ Microfinance(Github)/src/database/MyQueries.java
@@ -0,0 +1,131 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+import javax.swing.table.DefaultTableModel;
+
+import javax.swing.JOptionPane;
+
+
+import database.DBConnection;
+
+public class MyQueries {
+	static Connection con = null;
+	static Statement stmt;
+	static String query,query1;
+	ResultSet rs;
+	DBConnection connect = new DBConnection();
+	
+	public MyQueries() {
+		try {
+			con = connect.GetMySQLConnection();
+		}catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}catch (SQLException e) {
+			System.out.println(e);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+///////////////////// Client Query Start ///////////////////////
+	public DefaultTableModel getAllClient() {
+		DefaultTableModel dtm = new DefaultTableModel();
+		String strdataitem[]= new String[5];
+		try {
+			con=DBConnection.GetMySQLConnection();
+			stmt = con.createStatement();
+			String str ="Select * from Client";
+			ResultSet rs = stmt.executeQuery(str);
+			while (rs.next()) {
+				strdataitem[0] = rs.getString("ClientID");
+				strdataitem[1] = rs.getString("Name");
+				strdataitem[2] = rs.getString("NRC");
+				strdataitem[3] = rs.getString("Address");
+				strdataitem[4] = rs.getString(5);
+				dtm.addRow(strdataitem);
+			}
+			return dtm;
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e);
+		}
+		
+		return dtm;
+		
+	}
+	
+	
+	
+///////////////////// Client Query End /////////////////////////
+		//Duplicate Method
+	public boolean IsDuplicate(String tbName, String[] data) {
+		if(tbName.equals("client")) {
+			query = "select * from client where NRC='"+data[0]+"'";
+		}		
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			if (rs.next()) {
+				return true;	
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(),"SQL Exception", JOptionPane.ERROR_MESSAGE);
+			return true;
+		}
+	}
+	
+	//Insert
+	public static boolean InsertData(String tbName, String[] data) {
+	if(tbName.equals("client")) {
+		query = "insert into client(ClientID,Name,NRC,Address,Phone,DateofBirth,Home,Job,Salary) "
+				+ "values('"+data[0]+"','"+data[1]+"','"+data[2]+"','"+data[3]+"','"+Integer.parseInt(data[4])+"','"+data[5]+"','"+Integer.parseInt(data[6])+"','"+data[7]+"','"+Integer.parseInt(data[8])+"')";
+	}
+	try {
+		stmt = con.createStatement();
+		System.out.println(query);
+		if(stmt.executeUpdate(query)==1) {
+			return true;
+		}else {
+			return false;
+		}
+	}catch (SQLException e) {
+		JOptionPane.showMessageDialog(null, e.getMessage());// TODO: handle exception
+		e.printStackTrace();
+		return false;
+	}
+	}
+///////////////////// Group Query Start ////////////////////////
+	
+	
+	
+	
+	
+///////////////////// Group Query End //////////////////////////
+	
+///////////////////// LoanRequest Query Start //////////////////
+	
+	
+	
+	
+///////////////////// LoanRequest Query End ////////////////////
+	
+///////////////////// GroupLoan Query Start ////////////////////
+	
+	
+	
+	
+///////////////////// GroupLoan Query End //////////////////////
+	
+
+}
+
+
+
+
