fcaa4f661b2cd704372acdc1cc1edf69fb7dcbca
Monojue <monojue.km@gmail.com>
2020-09-04 17:39:41 +0630

    test branch

--- Microfinance(Github)/src/database/MyOueries.java
+++ Microfinance(Github)/src/database/MyOueries.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.table.DefaultTableModel;
+
 import database.DBConnection;
 
 public class MyOueries {
@@ -27,7 +29,30 @@ public class MyOueries {
 	}
 	
 ///////////////////// Client Query Start ///////////////////////
-	
+	public DefaultTableModel getAllClient() {
+		DefaultTableModel dtm = new DefaultTableModel();
+		String strdataitem[]= new String[5];
+		try {
+			con=DBConnection.GetMySQLConnection();
+			stmt = con.createStatement();
+			String str ="Select * from Client";
+			ResultSet rs = stmt.executeQuery(str);
+			while (rs.next()) {
+				strdataitem[0] = rs.getString("ClientID");
+				strdataitem[1] = rs.getString("Name");
+				strdataitem[2] = rs.getString("NRC");
+				strdataitem[3] = rs.getString("Address");
+				strdataitem[4] = rs.getString(5);
+				dtm.addRow(strdataitem);
+			}
+			return dtm;
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e);
+		}
+		
+		return dtm;
+		
+	}
 	
 	
 	
