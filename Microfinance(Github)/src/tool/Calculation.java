10be3c89c3069bd93fb3609ee2860ea140f0bf9d
Lenovo <Lenovo@LAPTOP-BC1VMMLC>
2020-09-08 11:27:34 +0630

    Calculator Method Changed

--- Microfinance(Github)/src/tool/Calculation.java
+++ Microfinance(Github)/src/tool/Calculation.java
@@ -1,27 +1,36 @@
 package tool;
 
+import javax.swing.table.DefaultTableModel;
+
 public class Calculation {
 
 	
-	public static void calculator(int amount, int duration, double rate) {
+	public static DefaultTableModel calculator(int amount, int duration, double rate) {
 		int principal;
 		int interest;
-		System.out.println("No\tAmount\tPrincipal\tInterest\tInstallment");
 		principal = Math.round(amount/duration);
+		DefaultTableModel dtm = new DefaultTableModel(duration,5);
 		for (int i = 0; i < duration; i++) {
 			interest = (int) Math.round((amount*rate)/100);
-			System.out.print(String.valueOf(i+1)+"\t");
-			System.out.print(String.valueOf(amount)+"\t");
-			System.out.print(String.valueOf(principal)+"\t");
-			System.out.print(String.valueOf(interest)+"\t");
-			System.out.print(String.valueOf(principal+interest));
-			System.out.println();
+			dtm.setValueAt(String.valueOf(i+1), i, 0);
+			dtm.setValueAt(String.valueOf(amount), i, 1);
+			dtm.setValueAt(String.valueOf(principal), i, 2);
+			dtm.setValueAt(String.valueOf(interest), i, 3);
+			dtm.setValueAt(String.valueOf(principal+interest), i, 4);
 			amount = amount - principal;
 		}
-			
+		return dtm;
 	}
 	
 	public static void main(String[] args) {
-		calculator(1000000, 12, 2.33);
+		//calculator(1000000, 12, 2.33);
+//		interest = (int) Math.round((amount*rate)/100);
+//		System.out.print(String.valueOf(i+1)+"\t");
+//		System.out.print(String.valueOf(amount)+"\t");
+//		System.out.print(String.valueOf(principal)+"\t");
+//		System.out.print(String.valueOf(interest)+"\t");
+//		System.out.print(String.valueOf(principal+interest));
+//		System.out.println();
+//		amount = amount - principal;
 	}
 }
