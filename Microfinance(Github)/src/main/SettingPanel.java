72188370cacdc5c3b466b65b3cb1d3a24bbfb7ed
Lenovo <Lenovo@LAPTOP-BC1VMMLC>
2020-09-09 16:03:49 +0630

    Individual Loan Setting Work!

--- Microfinance(Github)/src/main/SettingPanel.java
+++ Microfinance(Github)/src/main/SettingPanel.java
@@ -9,14 +9,26 @@ import javax.swing.JToggleButton;
 import javax.swing.border.TitledBorder;
 import javax.swing.UIManager;
 import net.miginfocom.swing.MigLayout;
+import tool.Checking;
+import tool.MyDate;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import database.MyQueries;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class SettingPanel extends JPanel {
 	private JTextField Iduration;
@@ -31,14 +43,119 @@ public class SettingPanel extends JPanel {
 	private JButton Icancel;
 	private JButton GEdit;
 	private JButton Gcancel;
-
+	private JLabel noteISetting;
+	
+	MyQueries msql = new MyQueries();
 	/**
 	 * Create the panel.
 	 */
 	public SettingPanel() {
 		initialize();
 		fieldDisable();
-		
+		GetIData();
+		GetGData();
+	}
+	
+	public void GetIData() {
+		try {
+		String[] IData = msql.GetIndividualLoanSetting();
+		Iduration.setText(IData[0]);
+		Iinterval.setText(IData[1]);
+		Irate.setText(IData[2]);
+		Ifees.setText(IData[3]);
+		}
+		catch(NullPointerException e) {
+			String[] data = new String[1];
+			data[0] = java.time.LocalDate.now().toString();
+			System.out.println(data[0]);
+			msql.InsertData("Iloansetting", data);
+			GetIData();
+		}
+	}
+	//Group Loan Setting
+	public void GetGData() {
+		try {
+		String[] IData = msql.GetGroupLoanSetting();
+		Gduration.setText(IData[0]);
+		GInterval.setText(IData[1]);
+		Grate.setText(IData[2]);
+		Gfees.setText(IData[3]);
+		}
+		catch(NullPointerException e) {
+			String[] data = new String[1];
+			data[0] = java.time.LocalDate.now().toString();
+			System.out.println(data[0]);
+			msql.InsertData("Gloansetting", data);
+			GetGData();
+		}
+	}
+	
+	public boolean Icheck() {
+		//Duration
+		if(Checking.IsNull(Iduration.getText())) {
+			noteISetting.setText("* Duration must be filled");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Checking.IsLetter(Iduration.getText())) {
+			noteISetting.setText("* Duration must be number only");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Integer.parseInt(Iduration.getText())<9 || Integer.parseInt(Iduration.getText())>60) {
+			noteISetting.setText("* Duration must be between 9 and 60");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		//Interval
+		if(Checking.IsNull(Iinterval.getText())) {
+			noteISetting.setText("* Interval must be filled");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Checking.IsLetter(Iinterval.getText())) {
+			noteISetting.setText("* Interval must be number only");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Integer.parseInt(Iinterval.getText())<1 || Integer.parseInt(Iinterval.getText())>100) {
+			noteISetting.setText("* Interval must be between 1 and 100");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		//Interest Rate
+		if(Checking.IsNull(Irate.getText())) {
+			noteISetting.setText("* Interest Rate must be filled");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Checking.IsLetter(Irate.getText())) {
+			noteISetting.setText("* Interest Rate must be number only");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Float.parseFloat(Irate.getText())<1 || Float.parseFloat(Irate.getText())>30) {
+			noteISetting.setText("* Interest Rate must be between 1 and 30");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		//Service Fee
+		if(Checking.IsNull(Ifees.getText())) {
+			noteISetting.setText("* Service Fee must be filled");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Checking.IsLetter(Ifees.getText())) {
+			noteISetting.setText("* Service Fee must be number only");
+			noteISetting.setVisible(true);
+			return false;
+		}
+		if(Float.parseFloat(Ifees.getText())<1 || Float.parseFloat(Ifees.getText())>30) {
+			noteISetting.setText("* Service Fee must be between 1 and 30");
+			noteISetting.setVisible(true);
+			return false;
+		}
+			return true;
 	}
 	
 	public void fieldDisable() {
@@ -102,6 +219,12 @@ public class SettingPanel extends JPanel {
 		panel.add(lblNewLabel_2, "cell 1 3");
 		
 		Iduration = new JTextField();
+		Iduration.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				noteISetting.setVisible(false);
+			}
+		});
 		Iduration.setEditable(false);
 		panel.add(Iduration, "cell 3 3 5 1,growx,aligny center");
 		Iduration.setColumns(10);
@@ -124,6 +247,12 @@ public class SettingPanel extends JPanel {
 		panel.add(lblNewLabel_5, "cell 1 4");
 		
 		Iinterval = new JTextField();
+		Iinterval.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				noteISetting.setVisible(false);
+			}
+		});
 		Iinterval.setEditable(false);
 		panel.add(Iinterval, "cell 3 4 5 1,growx");
 		Iinterval.setColumns(10);
@@ -146,6 +275,12 @@ public class SettingPanel extends JPanel {
 		panel.add(lblNewLabel_3, "cell 1 5");
 		
 		Irate = new JTextField();
+		Irate.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				noteISetting.setVisible(false);
+			}
+		});
 		Irate.setEditable(false);
 		panel.add(Irate, "cell 3 5 5 1,growx");
 		Irate.setColumns(10);
@@ -168,6 +303,12 @@ public class SettingPanel extends JPanel {
 		panel.add(lblNewLabel_4, "cell 1 6");
 		
 		Ifees = new JTextField();
+		Ifees.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				noteISetting.setVisible(false);
+			}
+		});
 		Ifees.setEditable(false);
 		panel.add(Ifees, "cell 3 6 5 1,growx");
 		Ifees.setColumns(10);
@@ -189,15 +330,53 @@ public class SettingPanel extends JPanel {
 		IEdit = new JButton("Edit");
 		IEdit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(IEdit.getText()=="Edit") {
 				IndividualfieldEnable();
+				}
+				
+				else if(IEdit.getText()=="Save") {
+					boolean check = Icheck();
+					if(check) {
+						String[] INewSetting = new String[5];
+						INewSetting[0] = Iduration.getText();
+						INewSetting[1] = Iinterval.getText();
+						INewSetting[2] = Irate.getText();
+						INewSetting[3] = Ifees.getText();
+						INewSetting[4] = java.time.LocalDate.now().toString();
+						boolean update = msql.UpdateData("Iloansetting", INewSetting);
+						if(update) {
+							fieldDisable();
+							JOptionPane.showMessageDialog(null, "Individual Loan Setting is Updated!","Save Sucessfully!",JOptionPane.INFORMATION_MESSAGE);
+						}
+						else {
+							IndividualfieldEnable();
+							JOptionPane.showMessageDialog(null, "Failed to Save Loan New Request!","Cannot Saved",JOptionPane.INFORMATION_MESSAGE);
+						}
+					}
+					else if(!check) {
+						IndividualfieldEnable();
+					}
+				}
 			}
 		});
+		
+		noteISetting = new JLabel("* Error");
+		noteISetting.setForeground(Color.RED);
+		panel.add(noteISetting, "cell 1 7 7 1");
+		noteISetting.setVisible(false);
+		
+		JLabel noteGSetting = new JLabel("* Error");
+		noteGSetting.setForeground(Color.RED);
+		panel.add(noteGSetting, "cell 11 7 7 1");
+		noteGSetting.setVisible(false);
+		
 		panel.add(IEdit, "cell 1 8,growx");
 		
 		Icancel = new JButton("Cancel");
 		Icancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				fieldDisable();
+				GetIData();
 			}
 		});
 		panel.add(Icancel, "cell 2 8 3 1");
