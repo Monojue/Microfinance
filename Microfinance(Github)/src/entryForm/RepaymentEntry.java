9ca7c790cf03b19df64ce94560b874a5a5f95bfa
Monojue <35295853+Monojue@users.noreply.github.com>
2020-09-21 13:54:28 +0630

    Repayment Form

--- Microfinance(Github)/src/entryForm/RepaymentEntry.java
+++ Microfinance(Github)/src/entryForm/RepaymentEntry.java
@@ -1,54 +1,47 @@
 package entryForm;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.util.Hashtable;
-import java.util.Vector;
 
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.JTextComponent;
 
-import database.MyQueries;
-import database.UQueries;
 import net.miginfocom.swing.MigLayout;
-import tool.Calculation;
-import tool.MyString;
-import tool.Select;
-import java.awt.Button;
-import java.awt.TextArea;
-import java.awt.Panel;
 
-public class ViewDetails extends JFrame {
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.JScrollPane;
+
+public class RepaymentEntry extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JPanel CPanel;
 	private JTextField textCID;
 	private JTextField textPName;
 	private JTextField textPNRC;
+	private JTextField textPDOB;
 	private JTextField textPAddress;
 	private JTextField textPPhone;
-	private JTextField textPDOB;
 	private JCheckBox checkPHome;
 	private JTextField textPJob;
 	private JTextField textPSalary;
+	private JPanel GPanel;
 	private JTextField txtGroupID;
 	private JTextField txtLID;
 	private JTextField txtM1ID;
@@ -60,22 +53,14 @@ public class ViewDetails extends JFrame {
 	private JTextField txtM2Name;
 	private JTextField txtM1Name;
 	private JTextField txtLName;
-	private JPanel CPanel;
-	private JPanel GPanel;
 	private JTextField textID;
 	private JTextField textDate;
-	private JTextField textAmount;
-	private JLabel lblRate;
-	private String Rate;
-	private String Fees;
-	private JLabel lblFees;
-	private JLabel lblKyats;
-	private JTextField textDuration;
-	private TextArea textRemark;
-	private String LoanRequestID, ID, Amount, Duration;
-	
-	MyQueries msql = new MyQueries();
-	static UQueries usql = new UQueries();
+	private JTable table_1;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -84,7 +69,7 @@ public class ViewDetails extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewDetails frame = new ViewDetails(null,null,null,null,null);
+					RepaymentEntry frame = new RepaymentEntry("Group");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -95,135 +80,42 @@ public class ViewDetails extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @param duration 
-	 * @param amount 
-	 * @param groupID 
-	 * @param loanRequestID 
 	 */
-	public ViewDetails(String form, String loanRequestID, String groupID, String amount, String duration) {
+	public RepaymentEntry(String form) {
 		Initialize();
 		if (form.equals("Group")) {
 			GPanel.setVisible(true);
 			CPanel.setVisible(false);
-			LoanRequestID = loanRequestID;
-			ID = groupID;
-			Amount = amount;
-			Duration = duration;
-			GroupSetData();
+			
 		}
 		
 		else if (form.equals("Individual")) {
 			GPanel.setVisible(false);
 			CPanel.setVisible(true);
-			textID.setText(loanRequestID);
-			LoanRequestID = loanRequestID;
-			ID = groupID;
-			Amount = amount;
-			Duration = duration;
-			ClientSetData();
-		}
-		
-	}
-	
-	public boolean check() {
-		if(textRemark.getText()=="") {
-				JOptionPane.showMessageDialog(null, "Please fill a Remark!","Error!",JOptionPane.INFORMATION_MESSAGE);
-				return false;
-		}
-		else {
-			return true;
-		}
-	}
-	
-	public void ClientSetData() {
-		String[] ClientDetails = msql.getClientDetailsFormID(ID);
-		String[] LoanDetails = msql.GetLoanRequestData(LoanRequestID);
-		String[] Date = msql.GetLoanRequestedDate("Individual",LoanRequestID);
-		textDate.setText(Date[2]);
-		textID.setText(LoanRequestID);
-		lblRate.setText(LoanDetails[3]+" %");
-		textCID.setText(ID);
-		textPName.setText(ClientDetails[1]);
-		textPNRC.setText(ClientDetails[2]);
-		textPAddress.setText(ClientDetails[3]);
-		textPPhone.setText(ClientDetails[4]);
-		textPDOB.setText(ClientDetails[5]);
-		textPJob.setText(ClientDetails[7]);
-		textPSalary.setText(ClientDetails[8]);
-		String home = ClientDetails[6];
-		if (home.equals("1")) {
-			checkPHome.setSelected(true);
-		}
-		else {
-			checkPHome.setSelected(false);
-		}
-		textAmount.setText(Amount);
-		textDuration.setText(Duration);;
-	}
-	
-	public void GroupSetData() {
-		String[] GroupDetails = msql.getGroupDetailsFormID(ID);
-		String[] LoanDetails = msql.GetLoanRequestData(LoanRequestID);
-		String[] Date = msql.GetLoanRequestedDate("Group",LoanRequestID);
-		textDate.setText(Date[2]);
-		textID.setText(LoanRequestID);
-		lblRate.setText(LoanDetails[3]+" %");
-		txtGroupID.setText(GroupDetails[0]);
-		txtLName.setText(GroupDetails[6]);
-		txtM1Name.setText(GroupDetails[7]);
-		txtM2Name.setText(GroupDetails[8]);
-		txtM3Name.setText(GroupDetails[9]);
-		txtM4Name.setText(GroupDetails[10]);
-		txtLID.setText(GroupDetails[1]);
-		txtM1ID.setText(GroupDetails[2]);
-		txtM2ID.setText(GroupDetails[3]);
-		txtM3ID.setText(GroupDetails[4]);
-		txtM4ID.setText(GroupDetails[5]);
-		textAmount.setText(Amount);
-		textDuration.setText(Duration);
-	}
-
-	public void ViewClientDetails(String Ctype){
-		if(Ctype == "Leader") {
-			new ClientEntry(txtLID.getText(),"View").setVisible(true);
-		}
-		else if(Ctype == "M1") {
-			new ClientEntry(txtM1ID.getText(),"View").setVisible(true);
-		}
-		else if(Ctype == "M2") {
-			new ClientEntry(txtM2ID.getText(),"View").setVisible(true);
-		}
-		else if(Ctype == "M3") {
-			new ClientEntry(txtM3ID.getText(),"View").setVisible(true);
-		}
-		else if(Ctype == "M4") {
-			new ClientEntry(txtM4ID.getText(),"View").setVisible(true);
+			
 		}
 	}
 	
 	public void Initialize() {
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 10, 579, 741);
+		setBounds(100, 100, 1024, 691);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.LIGHT_GRAY);
-		panel.setBounds(10, 0, 1293, 664);
+		panel.setBounds(0, 0, 1008, 650);
 		this.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(10, 11, 536, 40);
+		panel_2.setBounds(10, 11, 986, 40);
 		panel.add(panel_2);
 		panel_2.setLayout(new MigLayout("", "[70.00px][126.00px][130.00,grow][40px][135.00px]", "[26px]"));
 		
-		JLabel lblLoanRequestId = new JLabel("Loan Request ID:");
+		JLabel lblLoanRequestId = new JLabel("Repayment ID:");
 		lblLoanRequestId.setBackground(Color.RED);
 		lblLoanRequestId.setForeground(Color.BLACK);
 		panel_2.add(lblLoanRequestId, "cell 0 0,alignx trailing,aligny center");
@@ -233,18 +125,20 @@ public class ViewDetails extends JFrame {
 		panel_2.add(textID, "cell 1 0,growx");
 		textID.setColumns(10);
 		
-		JLabel lblRequestDate = new JLabel("Request Date :");
+		JLabel lblRequestDate = new JLabel("Repayment Date:");
 		panel_2.add(lblRequestDate, "cell 3 0,alignx trailing,growy");
 		
 		textDate = new JTextField();
 		textDate.setEditable(false);
 		panel_2.add(textDate, "cell 4 0,growx");
 		textDate.setColumns(10);
-		
+//		
+//		//////////
 		CPanel = new JPanel();
-		CPanel.setBounds(10, 61, 536, 298);
-		CPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "CLIENT INFROMATION", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.add(CPanel);
+//		CPanel.setBounds(10, 61, 536, 298);
+		CPanel.setBounds(12, 58, 413, 298);
+		CPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "CLIENT INFROMATION", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		CPanel.setLayout(new MigLayout("", "[][68px][27.00][40px][4px][4px][4px][50px][8px][42px][8px][31px][8px][97px]", "[][30][20px][20px][20px][50][20px][20][20px][20px][]"));
 		
 		JLabel lblNewLabel_5 = new JLabel("CLIENT ID");
@@ -317,12 +211,12 @@ public class ViewDetails extends JFrame {
 		textPSalary.setEditable(false);
 		CPanel.add(textPSalary, "cell 3 9 5 1,alignx left,aligny top");
 		textPSalary.setColumns(10);
-//////////////////////////
+//		//////////////
 		GPanel = new JPanel();
+		panel.add(GPanel);
 		GPanel.setBackground(Color.WHITE);
 		GPanel.setBorder(new TitledBorder(null, "GROUP INFORMATION", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		GPanel.setBounds(10, 61, 536, 297);
-		panel.add(GPanel);
+		GPanel.setBounds(12, 58, 413, 297);
 		GPanel.setLayout(new MigLayout("", "[20:20:20][61px][166.00px][160.00px][]", "[30][30][30][30][30][30][30][::10][]"));
 		
 		JLabel lbl = new JLabel("Group ID");
@@ -351,11 +245,7 @@ public class ViewDetails extends JFrame {
 		GPanel.add(label_2, "cell 3 1,alignx center,aligny top");
 		
 		Button button = new Button("View");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				ViewClientDetails("Leader");
-			}
-		});
+		
 		GPanel.add(button, "cell 4 2");
 		
 		txtM1ID = new JTextField();
@@ -366,11 +256,7 @@ public class ViewDetails extends JFrame {
 		GPanel.add(txtM1ID, "cell 3 3,grow");
 		
 		Button button_1 = new Button("View");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				ViewClientDetails("M1");
-			}
-		});
+		
 		GPanel.add(button_1, "cell 4 3");
 		
 		txtM2ID = new JTextField();
@@ -382,7 +268,7 @@ public class ViewDetails extends JFrame {
 		Button button_2 = new Button("View");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				ViewClientDetails("M2");
+//				ViewClientDetails("M2");
 			}
 		});
 		GPanel.add(button_2, "cell 4 4");
@@ -396,7 +282,7 @@ public class ViewDetails extends JFrame {
 		Button button_3 = new Button("View");
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				ViewClientDetails("M3");
+//				ViewClientDetails("M3");
 			}
 		});
 		GPanel.add(button_3, "cell 4 5");
@@ -452,109 +338,69 @@ public class ViewDetails extends JFrame {
 		Button button_4 = new Button("View");
 		button_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
-				ViewClientDetails("M4");
+//				ViewClientDetails("M4");
 			}
 		});
 		GPanel.add(button_4, "cell 4 6");
-/////////////////////////////////////////////
-		JPanel panel_4 = new JPanel();
-		panel_4.setBounds(10, 364, 536, 108);
-		panel_4.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "LOAN INFORMATION", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.add(panel_4);
-		panel_4.setLayout(new MigLayout("", "[21.00][65px][81.00px][20.00px][77px][][][]", "[20px][14px][][]"));
-
 		
-		JLabel lblNewLabel_2 = new JLabel("Amount");
-		panel_4.add(lblNewLabel_2, "cell 1 0 1 2,alignx left");
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(437, 58, 559, 532);
+		panel.add(scrollPane);
 		
-		textAmount = new JTextField();
-		textAmount.setEditable(false);
-		textAmount.setForeground(Color.black);
-		textAmount.setFont(new Font("Tahoma", Font.BOLD, 13));
-		textAmount.setColumns(10);
-		panel_4.add(textAmount, "cell 2 0 2 2,growx");
+		table_1 = new JTable();
+		scrollPane.setViewportView(table_1);
 		
-		lblKyats = new JLabel("Kyats");
-		panel_4.add(lblKyats, "cell 4 0 1 2");
-		
-		label = new JLabel("Duration");
-		panel_4.add(label, "cell 5 0 1 2,alignx trailing");
-		
-		textDuration = new JTextField();
-		textDuration.setEditable(false);
-		textDuration.setColumns(10);
-		panel_4.add(textDuration, "cell 6 0 1 2,growx");
-		
-		label_1 = new JLabel("Month");
-		panel_4.add(label_1, "cell 7 0 1 2");
-		
-		JLabel lblNewLabel_4 = new JLabel("Interest Rate");
-		panel_4.add(lblNewLabel_4, "cell 1 2");
-		
-		
-		lblRate = new JLabel(Rate + "%");
-		lblRate.setForeground(Color.BLUE);
-		panel_4.add(lblRate, "cell 2 2");
-		
-		JLabel label_25 = new JLabel("Service Fees");
-		panel_4.add(label_25, "cell 1 3");
-		
-		lblFees = new JLabel(Fees + "%");
-		lblFees.setForeground(Color.BLUE);
-		panel_4.add(lblFees, "cell 2 3");
-		
-		textRemark = new TextArea();
-		textRemark.setBounds(10, 489, 536, 154);
-		panel.add(textRemark);
-		
-		Panel panel_1 = new Panel();
-		panel_1.setBounds(10, 649, 533, 33);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Payment Details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(12, 366, 413, 224);
 		panel.add(panel_1);
-		panel_1.setLayout(new MigLayout("", "[grow][][][]", "[]"));
+		panel_1.setLayout(new MigLayout("", "[][][][63.00][90.00]", "[10][30][30][30][30][30]"));
 		
-		JButton btnNewButton_1 = new JButton("Accept");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String[] Approve = new String[2];
-				Approve[0] = textID.getText();
-				Approve[1] = "1";
-				boolean update = msql.UpdateData("loanrequest", Approve);
-				if (update) {
-					JOptionPane.showMessageDialog(null, "New Loan Request is Approved Sucessfully!","Success!",JOptionPane.INFORMATION_MESSAGE);
-					dispose();
-				}
-				else if(!update) {
-					JOptionPane.showMessageDialog(null, "Failed to Approve Request!","Cannot Saved",JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
-		});
-		panel_1.add(btnNewButton_1, "cell 1 0");
+		JLabel lblNewLabel_2 = new JLabel("For Month");
+		panel_1.add(lblNewLabel_2, "cell 1 1");
 		
-		JButton btnNewButton = new JButton("Decline");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(check()) {
-				String[] Decline = new String[3];
-				Decline[0] = textID.getText();
-				Decline[1] = "2";
-				Decline[2] = textRemark.getText();
-				boolean update = msql.UpdateData("loanrequestDecline", Decline);
-				if (update) {
-					JOptionPane.showMessageDialog(null, "New Loan Request is Rejected Sucessfully!","Success!",JOptionPane.INFORMATION_MESSAGE);
-					dispose();
-				}
-				else if(!update) {
-					JOptionPane.showMessageDialog(null, "Failed to Approve Request!","Cannot Saved",JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
-				}
-		});
-		panel_1.add(btnNewButton, "cell 3 0");
+		textField = new JTextField();
+		panel_1.add(textField, "cell 3 1,growx");
+		textField.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Remarks:");
-		lblNewLabel_3.setFont(new Font("MS UI Gothic", Font.BOLD, 12));
-		lblNewLabel_3.setBounds(10, 474, 72, 13);
-		panel.add(lblNewLabel_3);
+		JLabel lblNewLabel_3 = new JLabel("Principal Oustanding");
+		panel_1.add(lblNewLabel_3, "cell 1 2");
+		
+		textField_1 = new JTextField();
+		panel_1.add(textField_1, "cell 3 2 2 1,growx");
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Principal");
+		panel_1.add(lblNewLabel_4, "cell 1 3");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		panel_1.add(textField_2, "cell 3 3 2 1,growx");
+		
+		JLabel lblInterest = new JLabel("Interest");
+		panel_1.add(lblInterest, "cell 1 4");
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		panel_1.add(textField_4, "cell 3 4 2 1,growx");
+		
+		JLabel lblNewLabel_6 = new JLabel("Installment");
+		panel_1.add(lblNewLabel_6, "cell 1 5");
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		panel_1.add(textField_3, "cell 3 5 2 1,growx");
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(12, 600, 984, 40);
+		panel_3.setLayout(new MigLayout("", "[grow][][][]", "[]"));
+		panel.add(panel_3);
+		
+		JButton btnNewButton_1 = new JButton("Pay Now");
+		panel_3.add(btnNewButton_1, "cell 1 0");
+		
+		JButton btnNewButton = new JButton("Cancel");
+		panel_3.add(btnNewButton, "cell 3 0");
 		
 	}
 }
